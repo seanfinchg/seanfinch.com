affa1877173a09569b84e965856783d331023ddf
Sean Finch <finch.s@northeastern.edu>
2024-02-06 23:50:28 -0800

    add contact email

--- src/App.tsx
+++ src/App.tsx
@@ -48,6 +48,9 @@ function App() {
         <p>
           <span className="footer-link" onClick={() => openLinkInNewTab('https://seanfinch.com')}>seanfinch.com</span> • Website coded with help from <span className="footer-link" onClick={() => openLinkInNewTab('https://asahoo.dev')}>asahoo.dev</span>
         </p>
+        <p>
+          <a className="footer-link" href="mailto:hello@seanfinch.com">hello@seanfinch.com</a>
+        </p>
       </footer>
     </>
   );
