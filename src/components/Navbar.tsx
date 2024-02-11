80e2879042384af8e5588cf10da78bc823273586
Sean Finch <finch.s@northeastern.edu>
2024-02-10 23:48:18 -0800

    fix miswritten code in Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,6 @@ import React from 'react';
 const Navbar: React.FC = () => {
   return (
     <>
-      <div className='flex flex-row flex-wrap justify-end w-full mx-6'>
-        <p className='mx-4 text-center m-auto hover:font-bold'>Test</p>
-      </div>
       <div className='navbar'>
         <p>Home</p>
         <p>Projects</p>
