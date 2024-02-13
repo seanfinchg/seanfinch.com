c132af86143eb2d61032d67c763745b31a6c893b
Sean Finch <finch.s@northeastern.edu>
2024-02-12 23:56:37 -0800

    work on styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  FaGithub,
-  FaInstagram,
-  FaReddit,
-  FaLinkedin,
-  FaSpotify,
-  FaYoutube,
-} from "react-icons/fa";
+import { FaGithub, FaInstagram, FaReddit, FaLinkedin, FaSpotify, FaYoutube } from "react-icons/fa";
 import { useTheme } from "../contexts/themeContext";
 
 interface SocialMediaButtonProps {
@@ -14,35 +7,24 @@ interface SocialMediaButtonProps {
   children: React.ReactNode;
 }
 
-const SocialMediaButton: React.FC<SocialMediaButtonProps> = ({
-  url,
-  children,
-}) => {
+const SocialMediaButton: React.FC<SocialMediaButtonProps> = ({ url, children }) => {
   const { theme } = useTheme();
-  const buttonClass = `flex justify-center mx-2 px-4 py-2 text-base font-medium border border-transparent rounded-lg hover:border-indigo-600 cursor-pointer transition-colors duration-200 ${theme === "light" ? "bg-ultra-light-mode text-ultra-dark-mode" : "bg-ultra-dark-mode text-light-mode"}`;
+  const buttonClass = `flex justify-center mx-2 px-4 py-2 text-base font-medium border border-transparent rounded-lg hover:border-indigo-600 cursor-pointer transition-colors duration-200 ${
+    theme === "light" ? "bg-ultra-light-mode text-ultra-dark-mode" : "bg-ultra-dark-mode text-light-mode"
+  }`;
 
-  const openLinkInNewTab = (url: string) => {
+  const openLinkInNewTab = () => {
     window.open(url, "_blank");
   };
 
-  return (
-    <button className={buttonClass} onClick={() => openLinkInNewTab(url)}>
-      {children}
-    </button>
-  );
+  return <button className={buttonClass} onClick={openLinkInNewTab}>{children}</button>;
 };
 
 function Home() {
   const { theme } = useTheme();
 
   return (
-    <div
-      className={`flex justify-center ${
-        theme === "light"
-          ? "bg-light-mode text-ultra-dark-mode"
-          : "bg-dark-mode text-light-mode"
-      }`}
-    >
+    <div className={`flex justify-center ${theme === "light" ? "bg-light-mode text-ultra-dark-mode" : "bg-dark-mode text-light-mode"}`}>
       <div className="w-full">
         <div className="flex flex-col items-center">
           <p className="mb-8 text-4xl font-extrabold">Sean Finch</p>
@@ -52,32 +34,16 @@ function Home() {
         </div>
         <div>
           <div className="my-2.5 flex justify-center">
-            <SocialMediaButton url="https://github.com/seanfinchg">
-              <FaGithub size={30} />
-            </SocialMediaButton>
-            <SocialMediaButton url="https://www.instagram.com/straight.up.sean/">
-              <FaInstagram size={30} />
-            </SocialMediaButton>
-            <SocialMediaButton url="https://www.reddit.com/user/mk7sean">
-              <FaReddit size={30} />
-            </SocialMediaButton>
-            <SocialMediaButton url="https://www.linkedin.com/in/sean-finch-21803927b/">
-              <FaLinkedin size={30} />
-            </SocialMediaButton>
-            <SocialMediaButton url="https://open.spotify.com/user/31zrqevhky5vln3wuz3uuixspku4">
-              <FaSpotify size={30} />
-            </SocialMediaButton>
-            <SocialMediaButton url="https://www.youtube.com/channel/UC-0Oz_dgX4-MzMO_KNH7XuA">
-              <FaYoutube size={30} />
-            </SocialMediaButton>
+            <SocialMediaButton url="https://github.com/seanfinchg"><FaGithub size={30} /></SocialMediaButton>
+            <SocialMediaButton url="https://www.instagram.com/straight.up.sean/"><FaInstagram size={30} /></SocialMediaButton>
+            <SocialMediaButton url="https://www.reddit.com/user/mk7sean"><FaReddit size={30} /></SocialMediaButton>
+            <SocialMediaButton url="https://www.linkedin.com/in/sean-finch-21803927b/"><FaLinkedin size={30} /></SocialMediaButton>
+            <SocialMediaButton url="https://open.spotify.com/user/31zrqevhky5vln3wuz3uuixspku4"><FaSpotify size={30} /></SocialMediaButton>
+            <SocialMediaButton url="https://www.youtube.com/channel/UC-0Oz_dgX4-MzMO_KNH7XuA"><FaYoutube size={30} /></SocialMediaButton>
           </div>
           <div className="my-2.5 flex justify-center">
-            <SocialMediaButton url="https://300021720.wixsite.com/aotaportfolio">
-              Music Portfolio
-            </SocialMediaButton>
-            <SocialMediaButton url="https://docs.google.com/document/d/19Yhwee36co-cGrTDsVJUwNw30nKb_iQjcCVFIlCAKtg/edit?usp=sharing">
-              Resume
-            </SocialMediaButton>
+            <SocialMediaButton url="https://300021720.wixsite.com/aotaportfolio">Music Portfolio</SocialMediaButton>
+            <SocialMediaButton url="https://docs.google.com/document/d/19Yhwee36co-cGrTDsVJUwNw30nKb_iQjcCVFIlCAKtg/edit?usp=sharing">Resume</SocialMediaButton>
           </div>
         </div>
       </div>
