e87005538cd8cbc4fdadae8324464ae51211d1d1
Sean Finch <finch.s@northeastern.edu>
2024-02-16 09:23:53 -0800

    fix Footer.tsx for smaller screens

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -12,11 +12,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <div
-      className={
-        theme === "light"
-          ? "bg-light-mode text-dark-mode min-h-screen flex flex-col"
-          : "bg-dark-mode text-light-mode min-h-screen flex flex-col"
-      }
+      className={`"flex flex-col min-h-screen justify-between ${theme === "light" ? "bg-light-mode text-dark-mode min-h-screen flex flex-col" : "bg-dark-mode text-light-mode min-h-screen flex flex-col"}`}
     >
       <Navbar />
       <div>
