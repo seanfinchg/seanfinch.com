ff4dc273fd09b678f76ac13079d7a76b18b397ad
Sean Finch <145179592+seanfinchg@users.noreply.github.com>
2024-05-29 22:14:31 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,8 +27,8 @@ const Footer: React.FC = () => {
         {getLinkElement("https://asahoo.dev", "asahoo.dev")}
         <div className="flex justify-center">
           <a href="https://www.buymeacoffee.com/seanfinch">
-            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee!&emoji=☕&slug=seanfinch&button_colour=FF5F5F&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00" width="200" height="auto" /><
-          /a>
+            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee!&emoji=☕&slug=seanfinch&button_colour=FF5F5F&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00" width="200" height="auto" />
+          </a>
         </div>
       </p>
     </footer>
