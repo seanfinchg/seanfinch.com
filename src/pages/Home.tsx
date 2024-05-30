5f9cfd17efa65345cd1fb07f47ccb63e088afadd
Sean Finch <145179592+seanfinchg@users.noreply.github.com>
2024-05-29 22:18:25 -0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -91,7 +91,9 @@ function Home() {
           </div>
           <div className="my-2.5 flex justify-center flex-wrap">
             <SocialMediaButton url="/resume.pdf">Resume</SocialMediaButton>
-            <a href="https://www.buymeacoffee.com/seanfinch"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee!&emoji=☕&slug=seanfinch&button_colour=FF5F5F&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00" /></a>
+            <a href="https://www.buymeacoffee.com/seanfinch">
+              <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee!&emoji=☕&slug=seanfinch&button_colour=FF5F5F&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00" width="200" height="auto" />
+            </a>
             <SocialMediaButton url="/music_resume.pdf">
               Music Resume
             </SocialMediaButton>
