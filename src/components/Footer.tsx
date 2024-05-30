3c97930fcf48faad51b89dd358d76649e028c7fd
Sean Finch <145179592+seanfinchg@users.noreply.github.com>
2024-05-29 22:12:44 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,14 +19,17 @@ const Footer: React.FC = () => {
 
   return (
     <footer
-      className={`relative bottom-0 py-4 w-full text-center items-center ${theme === "light" ? "bg-light-mode text-dark-mode" : "bg-dark-mode text-light-mode"}`}
+      className={`relative bottom-0 py-4 w-full text-center ${theme === "light" ? "bg-light-mode text-dark-mode" : "bg-dark-mode text-light-mode"}`}
     >
       <p>
         {getLinkElement("https://seanfinch.com", "seanfinch.com")} • Website
         coded with help from{" "}
         {getLinkElement("https://asahoo.dev", "asahoo.dev")}
-        <a href="https://www.buymeacoffee.com/seanfinch">
-        <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee!&emoji=☕&slug=seanfinch&button_colour=FF5F5F&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00" width="200" height="auto" /></a>
+        <div className="flex justify-center">
+          <a href="https://www.buymeacoffee.com/seanfinch">
+            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee!&emoji=☕&slug=seanfinch&button_colour=FF5F5F&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00" width="200" height="auto" /><
+          /a>
+        </div>
       </p>
     </footer>
   );
